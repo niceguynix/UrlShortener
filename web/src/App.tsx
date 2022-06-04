0e390c46dba48c8b267aaf551128e6a5a607246c
manoj <its200manoj@gmail.com>
2022-06-04 21:25:06 +0530

    add: basic layout

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,21 @@
-
+import { ChangeEventHandler, useState } from 'react';
+import './App.css'
 
 
 function App() {
-    return <>Hello World!</>;
+
+    const [input,setInput] = useState("");
+
+    const handleInputChange = (e: React.FormEvent<HTMLInputElement>)=> {
+        setInput(e.currentTarget.value);
+      };
+
+    return (
+        <div>
+        <input id ="input" onChange={handleInputChange} className="form-label" placeholder='Enter Url to Shorten' value={input} />
+        <button id="submitButton">Create Short URL</button>
+        </div>
+    )
 }
 
 export default App;
---
+++ web/src/Redirect.tsx
@@ -0,0 +1,8 @@
+import { useParams } from "react-router-dom";
+
+function Redirect() {
+  const params = useParams();
+  return <>"HI"</>;
+}
+
+export default Redirect;
